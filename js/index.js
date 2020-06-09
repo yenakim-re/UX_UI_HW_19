4a566f13b9d52192004118b746b3ddfc6ecc375a
yenakim-re <64682859+yenakim-re@users.noreply.github.com>
2020-06-09 15:45:37 -0700

    caseStudy1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,31 @@
 console.log("check if it's working");
 
+const sliders = document.querySelectorAll(".slide-in");
+const appearOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 300px 0px"
+};
+
+const appearonscroll= new IntersectionObserver
+ (function(
+  entries, 
+  appearonscroll
+  ){
+    entries.forEach(entry => {
+      if(!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add('appear');
+        appearonscroll.unobserve(entry.target);
+      }
+  })
+},
+  appearOptions);
+  
+ sliders.forEach(slider => {
+    appearonscroll.observe(slider);
+  });
+
 /* const faders = document.querySelectorAll(".fade-in");
 const appearOptions = {
   threshold: 1,
@@ -27,30 +53,6 @@ faders.forEach((fader) => {
   appearOnScroll.observe(fader);
 }); */
 
-const sliders = document.querySelectorAll(".slide-in");
-const appearOptions = {
-  threshold: 1,
-  rootMargin: "0px 0px 300px 0px"
-};
 
-const appearonscroll= new IntersectionObserver
- (function(
-  entries, 
-  appearonscroll
-  ){
-    entries.forEach(entry => {
-      if(!entry.isIntersecting) {
-        return;
-      } else {
-        entry.target.classList.add('appear');
-        appearonscroll.unobserve(entry.target);
-      }
-  })
-},
-  appearOptions);
-  
- sliders.forEach(slider => {
-    appearonscroll.observe(slider);
-  });
 
 
