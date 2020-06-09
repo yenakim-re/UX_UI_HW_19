0679407dc74777756f5edb2922abcb779724f921
yenakim-re <64682859+yenakim-re@users.noreply.github.com>
2020-06-08 21:19:19 -0700

    jQuery (all hover), logo v2

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
-console.log("Testing index.js file");
-
+$('.header-overlay').css({
+    opacity: function() {
+      var elementHeight = $(this).height(),
+          opacity = ((1 - (elementHeight - scrollTop) / elementHeight) * 0.8) + 0.2;
+  
+      return opacity;
+    }
+  });
