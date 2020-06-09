d5989dd6b9d8cea6707f9b90b149cb02814f47dc
yenakim-re <64682859+yenakim-re@users.noreply.github.com>
2020-06-09 14:56:23 -0700

    scrolling/fading

--- js/index.js
+++ js/index.js
@@ -1,8 +1,29 @@
-$('.header-overlay').css({
-    opacity: function() {
-      var elementHeight = $(this).height(),
-          opacity = ((1 - (elementHeight - scrollTop) / elementHeight) * 0.8) + 0.2;
-  
-      return opacity;
+const faders = document.querySelectorAll(".fade-in");
+const appearOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 300px 0px",
+};
+
+const appearonscroll = new IntersectionObserver(function (
+  entries,
+  appearonscroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearonscroll.unobserve(entry.target);
     }
+
+    const appearOptions = {};
   });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+
+
+
